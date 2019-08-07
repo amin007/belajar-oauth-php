ebebbf9f55772185d847a089899af538f91b12e0
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-08-07 16:18:23 +0800

    tambah icon font awesome github

--- docs/github/ujilocal000/index.php
+++ docs/github/ujilocal000/index.php
@@ -82,8 +82,9 @@ if(session('access_token'))
 else
 {
 	echo '<h3>Not logged in</h3>';
-	echo '<p><a href="?action=login"
-	class="btn btn-secondary btn-block text-white">Log In</a></p>';
+	echo '<p><a href="?action=login"'
+	. 'class="btn btn-secondary btn-block text-white">Log In Github '
+	. '<i class="fab fa-github"></i></a></p>';
 }
 #------------------------------------------------------------------------------------------
 
