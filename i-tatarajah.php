737a3009f76e1c15187fff6434fec6ea5ee46c35
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-08-07 10:28:42 +0800

    tutup kejap koding utama

--- i-tatarajah.php
+++ i-tatarajah.php
@@ -1,4 +1,5 @@
 <?php
+/*
 # 4 folder utama
 define('KAWAL', 'Aplikasi/Kelas/Utama/Kawal');
 define('TANYA', 'Aplikasi/Kelas/Utama/Tanya');
@@ -8,6 +9,7 @@ define('FUNGSI', 'Aplikasi/Fungsi');
 
 # Fungsi Global
 require FUNGSI . '/Fungsi.php';
+*/
 
 # Sentiasa menyediakan garis condong di belakang (/) pada hujung jalan
 define('URL', dirname('http://' . $_SERVER['SERVER_NAME'] . $_SERVER['PHP_SELF']) . '/');
