570297858841f4fc1eece6da99b5b124b05b6944
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-08-08 14:13:00 +0800

    tutup komen include facebook sdk

--- docs/facebook/fbConfig.php
+++ docs/facebook/fbConfig.php
@@ -1,8 +1,8 @@
 <?php
 //echo '<hr><h2>FB Config</h2><hr>';
-/*
+
 # Include the autoloader provided in the SDK
-require_once __DIR__ . '/facebook-php-graph-sdk/autoload.php';
+//require_once __DIR__ . '/facebook-php-graph-sdk/autoload.php';
 
 # Include required libraries
 use Facebook\Facebook;
